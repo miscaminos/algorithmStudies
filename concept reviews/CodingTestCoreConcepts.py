ef4d415d8bd27290b296813e5b05a0d68b2097f1
miscaminos <sojung1221@gmail.com>
2021-04-09 02:44:54 +0900

    practiced solving greedy algorithm problem

--- concept reviews/CodingTestCoreConcepts.py
+++ concept reviews/CodingTestCoreConcepts.py
@@ -352,3 +352,36 @@ class CodingTestCoreConcepts:
         solution(K)
         for i in range(1, V+1):
             print("INF" if distance[i] == INF else distance[i])
+
+
+##############################################################################
+        # concept: Greedy algorithm (탐욕법)
+        # Greedy algorithm: 여러 경우 중 하나를 선택할때 
+        # 가장 좋다고 생각하는것을 선택해 나가면 결과적으로도 최선이 될것이다
+
+        # problem: 회의실 배정
+        # 일찍 끝나는 회의를 선택하면, 결과적으로 가장 많은 회의 가능
+        # 주의: 회의의 시작시간과 끝나는 시간이 같을 수 있다.
+
+        import sys
+
+        N = int(sys.stdin.readline())
+        meetings = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+        # 끝나는 시간 기준으로 정렬
+        #--> meetings.sort(key = lambda x:x[1])
+
+        #만약 동일한 끝시간이 있다면, 시작시간이 빠른것부터 정렬
+        #람다식 설명: x[1]를 기준으로 먼저 정렬, 그다음에 x[0]를 기준으로 정렬
+        #--> meetings.sort(key = lambda x:(x[1], x[0]))
+
+        #solution:
+        def solution(N,meetings):
+            meetings.sort(key = lambda x:(x[1], x[0]))
+            cnt = 0
+            end_time =0
+            for meeting in meetings:
+                if meeting[0] >= end_time:
+                    end_time = meeting[1]
+                    cnt+=1
+            return cnt
