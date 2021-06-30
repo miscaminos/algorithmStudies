68620be74954ca418832947282ec1e57d50362bb
miscaminos <78219019+miscaminos@users.noreply.github.com>
2021-06-30 19:12:48 +0900

    Add files via upload

---
+++ practice problems/StackQueue001.java
@@ -0,0 +1,37 @@
+package self.study.stackAndQueue;
+
+import java.util.*;
+
+public class StackQueue001{
+
+	/* Problem1:
+	 * 괄호가 입력되면 올바른 괄호이면 “YES", 올바르지 않으면 ”NO"를 출력합니다.
+	 * (())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()()))은 올바른 괄호가 아니다.
+	 * input:
+	 * 첫 번째 줄에 괄호 문자열이 입력됩니다. 문자열의 최대 길이는 30이다.
+	 * output:
+	 * 첫 번째 줄에 YES, NO를 출력한다.
+	 */
+	
+	//solution:
+	public String solution(String str){
+		String answer="YES";
+		Stack<Character> stack=new Stack<>();
+		for(char x : str.toCharArray()){
+			if(x=='(') stack.push(x);
+			else{
+				if(stack.isEmpty()) return "NO";
+				stack.pop();
+			}
+		}
+		if(!stack.isEmpty()) return "NO";
+		return answer;
+	}
+
+	public static void main(String[] args){
+		StackQueue001 T = new StackQueue001();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.println(T.solution(str));
+	}
+}
