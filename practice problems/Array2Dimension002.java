4a412e5ed8810e8d589c5cf14eeb9d1f311ee19c
miscaminos <78219019+miscaminos@users.noreply.github.com>
2021-07-01 14:13:36 +0900

    Update Array2Dimension002.java

--- practice problems/Array2Dimension002.java
+++ practice problems/Array2Dimension002.java
@@ -9,7 +9,7 @@ public class Array2Dimension002{
 	 * 각 격자판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다. 
 	 * 봉우리 지역이 몇 개 있는 지 알아내는 프로그램을 작성하세요.
 	 * 격자의 가장자리는 0으로 초기화 되었다고 가정한다.
-	 * 약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.
+	 * 만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.
 	 * 
 	 * 입력 condition:
 	 * 첫 줄에 자연수 N이 주어진다.(2<=N<=50)
@@ -18,17 +18,34 @@ public class Array2Dimension002{
 	
 	//my solution:
 	//need to find a better way to locate the points above/below/left/right of each index in the map.
-	public int solution(int n, int[][]map) {
-		int ans =0;
-		for(int i=1; i<n+1; i++) {
-			for(int j=1; j<n+1; j++) {
-				int[] tmp= {map[i-1][j], map[i+1][j], map[i][j-1], map[i][j+1]};
-				for(int t:tmp) {
-					if (map[i][j] > t) ans++;
+	public int solution2(int n, int [][]given) {
+		int[][]map = new int[n+2][n+2];
+		for (int k=0; k<n+2; k++) {			
+			map[0][k] = 0;
+			map[k][0] = 0;
+			map[n+1][k] = 0;
+			map[k][n+1] = 0;
+		}
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				map[i][j] = given[i-1][j-1];
+			}
+		}
+		
+		int answer = 0;
+		for (int i=1; i<n+1; i++) {
+			for (int j=1; j<n+1; j++) {
+				int m = map[i][j];
+				System.out.println(m);
+				if(m>map[i-1][j] && m>map[i+1][j] && 
+						m>map[i][j-1] && m>map[i][j+1]) {
+					answer++;
 				}
 			}
 		}
-		return ans;
+		System.out.println("number of highpoints: " + answer);
+		return answer;
 	}
 	
 	//teacher's solution:
