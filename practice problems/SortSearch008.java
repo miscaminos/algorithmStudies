d63b68d90b4a0f416ec00d6e26540925b05330e0
miscaminos <78219019+miscaminos@users.noreply.github.com>
2021-09-04 23:33:27 +0900

    Add files via upload

---
+++ practice problems/SortSearch008.java
@@ -0,0 +1,39 @@
+﻿package self.study.sortAndSearch;
+
+import java.util.*;
+
+public class SortSearch008{
+	
+	public int solution(int n, int m, int[] arr){
+		int answer = 0;
+		Arrays.sort(arr);
+		int lt = 0;
+		int mid = 0;
+		int rt = n-1;
+		while(lt <= rt){
+			mid = (lt+rt)/2;
+			if(arr[mid] == m){
+				answer = mid+1;
+				break;
+			}
+			if(arr[mid]>m) rt = mid-1;
+			else lt = mid+1;
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		SortSearch008 T = new SortSearch008();
+		Scanner sc = new Scanner(System.in);
+
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int[] arr=new int[n];
+
+		for(int i=0; i<n; i++){
+			arr[i]=sc.nextInt();
+		} 
+		System.out.println(T.solution(n, m, arr));
+	}
+
+}
