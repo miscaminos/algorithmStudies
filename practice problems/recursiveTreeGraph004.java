a6fcfbe28d76a0f62131284fc1f9654d8ca9846f
miscaminos <78219019+miscaminos@users.noreply.github.com>
2021-09-22 23:01:09 +0900

    Add files via upload

---
+++ practice problems/recursiveTreeGraph004.java
@@ -0,0 +1,19 @@
+package self.study.recursiveTreeGraph;
+
+import java.util.*;
+
+public class recursiveTreeGraph004{
+	
+	//use recursive to create Fibonacci pattern
+	public int DFS(int n){
+		if(n==1) return 1;
+		else if(n==2) return 1;
+		else return DFS(n-2)+DFS(n-1);
+	}
+	public static void main(String[] args){
+		recursiveTreeGraph004 T = new recursiveTreeGraph004();
+		int n=10;
+		for(int i=1; i<=n; i++) System.out.print(T.DFS(i)+" ");
+	}	
+
+}
